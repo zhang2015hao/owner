85fc447a7bd096a9484a9ecb0924b0ddca4d9e1f
zhanghao <690378361@163.com>
2017-07-18 09:22:11 +0800

    delete something

--- Test/src/main/springboot/CoreStyle2.java
+++ Test/src/main/springboot/CoreStyle2.java
@@ -1,6 +1,5 @@
 package main.springboot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
