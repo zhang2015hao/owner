678df8a36a980cc0aa6e74c3391689395071587d
zhanghao <690378361@163.com>
2017-07-19 11:21:59 +0800

    test

--- Test/src/main/redistemplate/MainTestRedisDeml.java
+++ Test/src/main/redistemplate/MainTestRedisDeml.java
@@ -1,11 +1,12 @@
 package main.redistemplate;
 
-
+import redis.clients.jedis.Jedis;
 
 public class MainTestRedisDeml {
 
 	public static void main(String[] args) {
-		
+		Jedis jedis = new Jedis("192.168.1.171");
+		jedis.asking();
 		
 	}
 }
